4134d2d5558fe3a92373a3e04b1c141687a39b0f
Camilo Castellar <camilocastelar3@gmail.com>
2022-06-16 16:34:36 -0500

    sleep in

---
+++ src/SleepIn.java
@@ -0,0 +1,28 @@
+/*  sleepIn => https://codingbat.com/prob/p187868
+ * The parameter weekday is true if it is a weekday
+ * and the parameter vacation is true if we are on vacation. 
+ * We sleep in if it is not a weekday or we're on vacation. 
+ * Return true if we sleep in.
+ * 
+    sleepIn(false, false) → true
+    sleepIn(true, false) → false
+    sleepIn(false, true) → true
+ */
+
+
+
+public class SleepIn {
+    public static void main(String[] args) {
+        Sleep sleep = new Sleep();
+        System.out.println(sleep.sleepIn(false, false));
+    }
+}
+
+class Sleep {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (weekday == true && vacation != true) {
+          return false;
+        }
+        return true;
+    }
+}
