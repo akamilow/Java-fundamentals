8a6aa3a4e7b3e729664f3738306be0b9af88543d
Camilo <camilocastelar3@gmail.com>
2023-11-01 12:45:26 -0500

    Ejercicio5

--- src/UdeCUnidadTres/Ejercicio2.java
+++ src/UdeCUnidadTres/Ejercicio2.java
@@ -1,12 +1,24 @@
 package UdeCUnidadTres;
+
 /*
 Desarrollar un programa que por medio de una función calcule la potencia de un número ingresado por parámetro.
  */
 public class Ejercicio2 {
-    
+
     static Integer calPotencia(int base, int exponente) {
-        return (int) Math.pow(base, exponente);
+        int resultado = 1;
+        if (base > 0 && exponente == 0) {
+            return resultado;
+        } else if (base == 0 && exponente >= 1) {
+            return 0;
+        } else {
+            for (int i = 1; i <= exponente; i++) {
+                resultado = resultado * base;
+            }
+        }
+        return resultado;
     }
+
     public static void main(String[] args) {
         System.out.println(calPotencia(2, 3));
     }
---
+++ src/UdeCUnidadTres/Ejercicio5.java
@@ -0,0 +1,12 @@
+package UdeCUnidadTres;
+/*
+Crear un programa que permita calcular la raíz cuadrada de un numero
+ */
+public class Ejercicio5 {
+    static double raizCuadrada(double numero){
+        return Math.sqrt(numero);
+    }
+    public static void main(String[] args) {
+        System.out.println(raizCuadrada(9));
+    }
+}
