b2750b0bc90d4aafc42f542e3b8363095a2b4cf5
Camilo <camilocastelar3@gmail.com>
2023-11-01 19:47:45 -0500

    Ejercicio9

---
+++ src/UdeCUnidadTres/Ejercicio9.java
@@ -0,0 +1,17 @@
+package UdeCUnidadTres;
+/*
+Usando recursividad 
+Desarrollar un programa que por medio de una función calcule la potencia de un número ingresado por parámetro.
+ */
+public class Ejercicio9 {
+    static int potencia(int base, int exponente){
+        if (exponente == 0) {
+            return 1;
+        } else {
+            return base * potencia(base, exponente - 1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(potencia(2, 3));
+    }
+}
