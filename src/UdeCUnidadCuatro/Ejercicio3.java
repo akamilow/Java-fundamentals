0b6e332fecc937447fba87c65afba0f4dcd462f9
Camilo <camilocastelar3@gmail.com>
2023-11-21 17:02:57 -0500

    Ejercicios Comentados Unidad 4

--- src/UdeCUnidadCuatro/Ejercicio3.java
+++ src/UdeCUnidadCuatro/Ejercicio3.java
@@ -8,9 +8,11 @@ en otro los números impares comprendidos entre 1 y 100 e imprima esos
 */
 
 public class Ejercicio3 {
+    // Se crean dos ArrayList para almacenar números pares e impares respectivamente
     static ArrayList<Integer> arrPares = new ArrayList<Integer>();
     static ArrayList<Integer> arrImpares = new ArrayList<Integer>();
 
+    // Método para crear un arreglo de 101 elementos con valores del 0 al 100
     static int[] setArreglo() {
         int[] arreglo = new int[101];
         for (int i = 0; i < arreglo.length; i++) {
@@ -19,18 +21,22 @@ public class Ejercicio3 {
         return arreglo;
     }
 
+    // Método para separar números pares e impares y almacenarlos en los ArrayList correspondientes
     static void getNums(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
+                // Si el número es par, se agrega al ArrayList de números pares
                 arrPares.add(arr[i]);
                 System.out.println("Es par: "+arr[i]);
-            } else if (!(arr[i] % 2 == 0)) {        
+            } else { 
+                // Si el número es impar, se agrega al ArrayList de números impares       
                 arrImpares.add(arr[i]);
                 System.out.println("Es impar: "+arr[i]);
             }
         }
     }
 
+    // Métodos para obtener los ArrayList de números pares e impares
     static ArrayList<Integer> getArrPares() {
         return arrPares;
     }
--- src/UdeCUnidadCuatro/Ejercicio4.java
+++ src/UdeCUnidadCuatro/Ejercicio4.java
@@ -5,6 +5,7 @@ package UdeCUnidadCuatro;
  */
 public class Ejercicio4 {
 
+    // Método para inicializar y llenar el primer arreglo con valores del 0 al 10
     static int[] setArregloUno() {
         int[] arregloUno = new int[11];
         for (int i = 0; i < arregloUno.length; i++) {
@@ -13,6 +14,7 @@ public class Ejercicio4 {
         return arregloUno;
     }
 
+    // Método para inicializar y llenar el segundo arreglo con valores del 0 al 10
     static int[] setArregloDos() {
         int[] arregloDos = new int[11];
         for (int i = 0; i < arregloDos.length; i++) {
@@ -21,17 +23,19 @@ public class Ejercicio4 {
         return arregloDos;
     }
 
+    // Método para sumar los elementos de dos arreglos y devolver la suma total
     static int sumArreglos(int[] arr1, int[] arr2) {
         int count = 0;
         for (int i = 0; i < arr1.length; i++) {
-            count += arr1[i];
-            count += arr2[i];
+            count += arr1[i];// Se suma el valor del elemento en la posición 'i' del primer arreglo
+            count += arr2[i];// Se suma el valor del elemento en la posición 'i' del segundo arreglo
+
         }
-        return count;
+        return count;// Retorna la suma total de ambos arreglos
     }
     public static void main(String[] args) {
-        int[] arr1 = setArregloUno();
-        int[] arr2 = setArregloDos();
-        System.out.println(sumArreglos(arr1, arr2));
+        int[] arr1 = setArregloUno();// Se inicializa el primer arreglo
+        int[] arr2 = setArregloDos();// Se inicializa el segundo arreglo
+        System.out.println(sumArreglos(arr1, arr2));// Se muestra en consola la suma total de ambos arreglos
     }
 }
